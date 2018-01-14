b32cde12b3dd9e8577352e6319d5cefa87f7728f
pfolus <kamilnowak91@wp.eu>
2018-01-14 17:11:30 +0100

    Add arguments handling in main app class

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -2,7 +2,25 @@ package com.codecool.scc;
 
 public class ConverterApplication {
 
+    public enum Option {JSON, XML}
+
     public static void main (String[] args) {
 
+        try{
+            Option choice = Option.valueOf(args[0]);
+
+            switch (choice) {
+                case JSON:
+                    System.out.println("json");
+                    break;
+                case XML:
+                    System.out.println("xml");
+                    break;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("No input file defined");
+        } catch (IllegalArgumentException e) {
+            System.out.println("wrong parameter given");
+        }
     }
 }
