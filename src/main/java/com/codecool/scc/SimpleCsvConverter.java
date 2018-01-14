4a1f9566ce3e32871d9ee174ee2f0006201ad5f8
pfolus <kamilnowak91@wp.eu>
2018-01-14 20:21:17 +0100

    Fix convert method name in simpleCsvConverter

--- src/main/java/com/codecool/scc/SimpleCsvConverter.java
+++ src/main/java/com/codecool/scc/SimpleCsvConverter.java
@@ -2,11 +2,11 @@ package com.codecool.scc;
 
 public class SimpleCsvConverter {
 
-    public void converter(String file, String outputFormat) {
-        System.out.println("i convert CSV to output format");
+    public void convert(String file, String outputFormat) {
+        System.out.println(String.format("i convert CSV to output format %s", outputFormat));
     }
 
-    public void converter(String file) {
+    public void convert(String file) {
         System.out.println("i convert CSV to default output format");
     }
 }
