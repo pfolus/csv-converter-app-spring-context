4404b5ad56522f71fadcc0e30851623899b440ed
pfolus <kamilnowak91@wp.eu>
2018-01-14 19:39:52 +0100

    Add simpleCsvConverter class

---
+++ src/main/java/com/codecool/scc/SimpleCsvConverter.java
@@ -0,0 +1,12 @@
+package com.codecool.scc;
+
+public class SimpleCsvConverter {
+
+    public void converter(String file, String outputFormat) {
+        System.out.println("i convert CSV to output format");
+    }
+
+    public void converter(String file) {
+        System.out.println("i convert CSV to default output format");
+    }
+}
