11626ba554301adb59ccf488d16bd4a30dbbb996
pfolus <kamilnowak91@wp.eu>
2018-01-21 21:22:19 +0100

    Fix CsvConverter

--- src/main/java/com/codecool/scc/SimpleCsvConverter.java
+++ src/main/java/com/codecool/scc/SimpleCsvConverter.java
@@ -1,12 +1,30 @@
 package com.codecool.scc;
 
+import com.codecool.scc.view.OutputFormatter;
+import com.codecool.scc.view.TableOutputFormatter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class SimpleCsvConverter {
 
-    public void convert(String file, String outputFormat) {
-        System.out.println(String.format("i convert CSV to output format %s", outputFormat));
+    FileReader reader;
+    OutputFormatterFactory factory = new OutputFormatterFactory();
+
+    public SimpleCsvConverter(FileReader reader){
+        this.reader = reader;
     }
 
-    public void convert(String file) {
-        System.out.println("i convert CSV to default output format");
+    public void convert(File file, ConverterApplication.Option outputFormat) throws FileNotFoundException {
+        List<String[]> data = reader.readData(file);
+        OutputFormatter outputFormatter = factory.createByFormat(outputFormat);
+        outputFormatter.printToConsole(data);
+    }
+
+    public void convert(File file) throws FileNotFoundException {
+        List<String[]> data = reader.readData(file);
+        OutputFormatter outputFormatter = new TableOutputFormatter();
+        outputFormatter.printToConsole(data);
     }
 }
