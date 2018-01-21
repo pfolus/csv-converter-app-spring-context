7dee258c4ba89ece367bf0995f7398d558c45aa4
pfolus <kamilnowak91@wp.eu>
2018-01-21 21:21:31 +0100

    Add OutputFormatter factory

---
+++ src/main/java/com/codecool/scc/OutputFormatterFactory.java
@@ -0,0 +1,18 @@
+package com.codecool.scc;
+
+import com.codecool.scc.view.JsonOutputFormatter;
+import com.codecool.scc.view.OutputFormatter;
+import com.codecool.scc.view.TableOutputFormatter;
+import com.codecool.scc.view.XmlOutputFormatter;
+
+public class OutputFormatterFactory {
+
+    public OutputFormatter createByFormat(ConverterApplication.Option outputFormat) {
+
+        if(outputFormat.name().equals("XML")) {
+            return new XmlOutputFormatter();
+        } else if (outputFormat.name().equals("JSON")) {
+            return new JsonOutputFormatter();
+        } return new TableOutputFormatter();
+    }
+}
