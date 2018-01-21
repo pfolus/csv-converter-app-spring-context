36f9783df1a5ede5722290808961f079eb73ff91
pfolus <kamilnowak91@wp.eu>
2018-01-21 21:20:43 +0100

    Add FileReader handling

--- src/main/java/com/codecool/scc/FileReader.java
+++ src/main/java/com/codecool/scc/FileReader.java
@@ -1,8 +1,24 @@
 package com.codecool.scc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileReader {
 
-    public void readData(String file) {
-        System.out.println("data read");
+    public List<String[]> readData(File file) throws FileNotFoundException {
+
+        List<String[]> wordsList = new ArrayList<>();
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine()) {
+
+            wordsList.add(sc.nextLine().split(","));
+        }
+        sc.close();
+
+        return wordsList;
     }
 }
