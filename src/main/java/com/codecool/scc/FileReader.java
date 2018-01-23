c69c86e7a6047ac9386cbc74f2254263c17be590
pfolus <kamilnowak91@wp.eu>
2018-01-23 16:14:50 +0100

    Add Component annotation to FileReader class

--- src/main/java/com/codecool/scc/FileReader.java
+++ src/main/java/com/codecool/scc/FileReader.java
@@ -1,11 +1,14 @@
 package com.codecool.scc;
 
+import org.springframework.stereotype.Component;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+@Component
 public class FileReader {
 
     public List<String[]> readData(File file) throws FileNotFoundException {
